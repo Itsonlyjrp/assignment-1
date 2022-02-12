a3d8174f8d58fe715d0537fd432f0fbf1e2b8afa
Itsonlyjrp <99532313+Itsonlyjrp@users.noreply.github.com>
2022-02-12 16:32:03 +0530

    Assignment-1

---
+++ assignment-1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("Name: Jyotiranjan Prusty\n");
+printf("Regd no:2102060016\n");
+printf("Branch: Electrical and Electronics Engineering\n");
+printf("Hobby: Watching Cricket and memes\n");
+}
